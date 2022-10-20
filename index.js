aa793fdbdeff3fae4da49d190287748614a596b8
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2022-10-20 09:31:06 +0530

    added images

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ console.log(divs);
 function changeHome(event){
     
    
-    
-    
-
+    let pic = document.querySelector("#photo");
+    if(event.target.parentElement.id == "work-icon-holder"){
+        pic.src ="chopta2.jpg";
+    }
+    else if(event.target.parentElement.id == "home-icon-holder"){
+        pic.src ="chopta.jpg";
+    }
+    else if(event.target.parentElement.id == "blog-icon-holder"){
+        pic.src ="chopta1.jpg";
+    }
+    else if(event.target.parentElement.id == "aboutUs-icon-holder"){
+        pic.src ="srinagar-uk.jpg";
+    }
+   // console.log(event.target.parentElement.id);
     for(var i = 0;i<divs.length;i++){
         if(divs[i].className.includes(" active")){
             divs[i].className = divs[i].className.replace(" active","")
